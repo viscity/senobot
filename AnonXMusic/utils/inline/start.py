cd503f0fe2a1e6d318308e9a86450f76dfd1e90d
krishnaji020210 <krishnaji020210@gmail.com>
2026-04-01 18:53:53 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -1,3 +1,4 @@
+from pyrogram.enums import ButtonStyle
 from pyrogram.types import InlineKeyboardButton
 
 import config
@@ -8,9 +9,15 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+                style=ButtonStyle.PRIMARY  # 🔵 Blue
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_CHAT,
+                style=ButtonStyle.SUCCESS  # 🟢 Green
+            ),
         ],
     ]
     return buttons
@@ -22,18 +29,42 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
+                style=ButtonStyle.PRIMARY
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(
+                text=_["S_B_4"],
+                callback_data="settings_back_helper",
+                style=ButtonStyle.DEFAULT
+            )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_6"],
+                user_id=config.OWNER_ID,
+                style=ButtonStyle.DANGER  # 🔴 Red
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_5"],
+                url=config.SUPPORT_CHANNEL,
+                style=ButtonStyle.SUCCESS
+            ),
         ],
-        [InlineKeyboardButton(text="• sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ •", url=f"https://github.com/xbitcode/music.git")],
-    
+        [
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_CHAT,
+                style=ButtonStyle.PRIMARY
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="˹δᴏᴜʀᴄє ᴄᴏᴅє˼",
+                url="https://files.catbox.moe/6jp8hr.mp4",
+                style=ButtonStyle.DANGER
+            )
+        ],
     ]
-    
+
     return buttons
