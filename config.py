0c96297caa4838cb5d92ca450fee176c78c8d35d
ㅤ𝛎 𝜄 ᥉ ᥴ᥆ ᥉ 𝜄 𝗍 𝛄 ✞ㅤ <166417113+viscity@users.noreply.github.com>
2026-04-03 00:36:07 +0530

    Updated  by Viscosity

--- config.py
+++ config.py
@@ -89,22 +89,23 @@ autoclean = []
 confirmer = {}
 file_cache: dict[str, float] = {}
 
-START_IMG_URL = ["https://files.catbox.moe/lt9dt0.jpg",
-                 "https://files.catbox.moe/4ui3jm.jpg"]
+START_IMG_URL = ["https://files.catbox.moe/1d28bt.jpg",
+                 "https://files.catbox.moe/v6s6jl.jpg",
+                 "https://files.catbox.moe/lbn948.jpg"]
     
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/iorcd7.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/4ui3jm.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/4ui3jm.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/4ui3jm.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/4ui3jm.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/4ui3jm.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/4ui3jm.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/4ui3jm.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/4ui3jm.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/4ui3jm.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/4ui3jm.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/un63rb.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/un63rb.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/un63rb.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/un63rb.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/un63rb.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/un63rb.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/un63rb.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/un63rb.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/un63rb.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/un63rb.jpg"
 
 
 
